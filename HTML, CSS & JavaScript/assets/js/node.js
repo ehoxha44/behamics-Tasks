800b52d1cd5ca5ce1c3c1d45fe2b2bb66582ae14
ehoxha44 <enishoxha10@icloud.com>
2022-08-15 00:46:27 +0200

    first commit

---
+++ HTML, CSS & JavaScript/assets/js/node.js
@@ -0,0 +1,30 @@
+let product = {
+    name: "VANS Ward Kinder Sneaker",
+    price: "39.90 CHF",
+    imageURL: "https://ochsnersport.scene7.com/asset/ochsnersport/product/tile/preset/v3-product-tile/w84/fit/fit-1/vans-ward-kinder-sneaker-1--01_1090001_P.jpg",
+    url: "https://www.ochsnersport.ch/de/shop/",
+    color: "black",
+}
+
+console.log(product)  
+
+let products = [
+    {
+        name: "Puma",
+        price: "49.90 CHF",
+    },
+    {
+        name: "ASICS",
+        price: "49.90 CHF",
+    },
+    {
+        name: "Kappa",
+        price: "29.90 CHF",
+    },
+    {
+        name: "BEACH MOUNTAIN",
+        price: "39.90 CHF",
+    },
+]
+
+console.log(products)
